4165fa1758b695bb1fdcb6fca86cd98d9611eae7
Boris Karaivanov <boriskaraivanov@yahoo.co.uk>
2014-10-18 18:23:23 +0000

    Set default login

--- Story/js/login.js
+++ Story/js/login.js
@@ -31,8 +31,8 @@ app.Login = (function () {
         };
 
         var show = function () {
-            $loginUsername.val('');
-            $loginPassword.val('');
+            $loginUsername.val('test');
+            $loginPassword.val('1234');
         };
 
         var getYear = function () {
